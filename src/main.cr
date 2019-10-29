a1edf55d29dd2f8230ce9d72344ceac9ea20f093
Åsmund Stavdahl <asmund.stavdahl@gmail.com>
2019-10-29 15:16:56 +0100

    catch exception while parsing command

--- src/main.cr
+++ src/main.cr
@@ -41,48 +41,53 @@ class InputSource
   end
 
   def next : Sudoku::Command | Iterator::Stop
-    if @queue.size >= 1
-      @queue.pop
-    else
-      input = @in_file.gets(true)
+    begin
+      if @queue.size >= 1
+        @queue.pop
+      else
+        input = @in_file.gets(true)
 
-      case input
-      when String
-        parts = input.split " "
-        if parts.first?
-          first = parts.shift.not_nil!
-          case
-          when first == "save"
-            case parts.size
-            when 0 then @queue.push Sudoku::Save.new SaveFile
-            else        @queue.push Sudoku::Save.new parts[0]
-            end
-          when first == "load"
-            case parts.size
-            when 0 then @queue.push Sudoku::Load.new SaveFile, 1
-            when 1 then @queue.push Sudoku::Load.new parts[0], 1
-            else        @queue.push Sudoku::Load.new parts[0], parts[1].to_i
-            end
-          when first =~ /^\d$/
-            digit = first.to_i
-            case parts.size
-            when 0
-              @queue.push Sudoku::Highlight.new(digit)
-            else
-              parts.each do |part|
-                x, y = part.split(",").map &.to_i
-                @queue.push Sudoku::Fill.new({x, y}, digit)
+        case input
+        when String
+          parts = input.split " "
+          if parts.first?
+            first = parts.shift.not_nil!
+            case
+            when first == "save"
+              case parts.size
+              when 0 then @queue.push Sudoku::Save.new SaveFile
+              else        @queue.push Sudoku::Save.new parts[0]
               end
+            when first == "load"
+              case parts.size
+              when 0 then @queue.push Sudoku::Load.new SaveFile, 1
+              when 1 then @queue.push Sudoku::Load.new parts[0], 1
+              else        @queue.push Sudoku::Load.new parts[0], parts[1].to_i
+              end
+            when first =~ /^\d$/
+              digit = first.to_i
+              case parts.size
+              when 0
+                @queue.push Sudoku::Highlight.new(digit)
+              else
+                parts.each do |part|
+                  x, y = part.split(",").map &.to_i
+                  @queue.push Sudoku::Fill.new({x, y}, digit)
+                end
+              end
+            else
+              puts "Command not recognized: #{input}"
+              @queue.push Sudoku::NoOp.new
             end
-          else
-            puts "Command not recognized: #{input}"
-            @queue.push Sudoku::NoOp.new
           end
+          @queue.pop.not_nil!
+        else stop
         end
-        @queue.pop.not_nil!
-      else stop
       end
     end
+  rescue ex
+    puts "#{typeof(ex)} #{ex}"
+    Sudoku::NoOp.new
   end
 end
 
